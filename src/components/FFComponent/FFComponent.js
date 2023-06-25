25e5f947a790b93d6f600a982e26a41a1bb4184e
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2023-06-25 21:39:35 +0300

    add: react import.

--- src/components/FFComponent/FFComponent.js
+++ src/components/FFComponent/FFComponent.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import classes from './FFComponent.module.css';
 
 export const FFComponent = () => {
--- src/components/JJComponent/JJComponent.js
+++ src/components/JJComponent/JJComponent.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import classes from './JJComponent.module.css';
 
 export const JJComponent = () => {
