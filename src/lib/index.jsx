c1531c9b30c07c2a64ee9ddb7e992e7450546491
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2023-07-04 18:32:16 +0300

    add: Cleanup.

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,23 +1,3 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-
-export default function HelloWorld(props) {
-    const {
-        greetee = 'World'
-    } = props
-
-    return (
-        <div>Hello, {greetee}!</div>
-    )
-}
-
-// See more about PropType validation here:
-// https://reactjs.org/docs/typechecking-with-proptypes.html
-
-HelloWorld.propTypes = {
-    greetee: PropTypes.string
-}
 
 export {Label} from '../../src/lib/components/atoms/Label/Label';
 export {Checkbox} from '../../src/lib/components/atoms/Checkbox/Checkbox';
